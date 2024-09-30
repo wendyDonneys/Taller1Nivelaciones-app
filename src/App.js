810b772a0ed633c7e26bd5e3a39bd25ce5e2efe8
wendyDonneys <wendydonneys@gmail.com>
2024-09-29 21:45:42 -0500

    cambios.
    Agregue valores diferentes al ejemplo del profe.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// obtengo el primer número , ejecutó un bucle si es menor a 100
+let firstNumber = parseInt(prompt(' > a 100):'));
+while (firstNumber <= 100) {
+  firstNumber = parseInt(prompt(' < a 100, Intenta otro: '));
 }
 
-export default App;
+// Obtengo el segundo número , ejecutó un bucle mientras no /5
+let secondNumber = parseInt(prompt('segundo número (/ 5):'));
+while (secondNumber % 5 !== 0) {
+  secondNumber = parseInt(prompt('/ 5, Ingresa otro: '));
+}
+
+// obtengo el tercer número, ejecutó un bucle, mientras (1² + 2)
+let thirdNumber = parseInt(prompt('Tercer Número 1²+2 ):'));
+while (thirdNumber !== (2 * firstNumber + secondNumber)) {
+  thirdNumber = parseInt(prompt('Eror, Ingresa otro: '));
+}
+
+alert('¡Lo lograste!');
